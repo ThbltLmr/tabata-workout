1811fe8e75d30b83b770fa4af9469b2bce87ca23
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-07-25 16:46:19 +0200

    moved timer and list to app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import MyButton from './components/Tabatatimer';
-import TabataTimer from './components/Tabatatimer';
-import Exercises from './components/Exercises';
+import { clear } from "@testing-library/user-event/dist/clear";
+import { useState, useEffect } from "react";
+import { Draggable, DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 function App() {
-  const userWorkout = [
+  const exercises = [
     {
       name: 'squats',
       thumbnail: './thumbnails/squats.png'
@@ -15,12 +15,60 @@ function App() {
       thumbnail: './thumbnails/pushups.png'
     }
   ]
+  const [seconds, setSeconds] = useState(10);
+  const [workout, setWorkout] = useState(false);
+  const [buttonMessage, setButtonMessage] = useState("Start");
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      clearInterval(interval);
+      if (buttonMessage === "Reset") {
+        if (seconds === 0) {
+          if (workout) {
+            setSeconds(10);
+            setWorkout(false);
+          } else {
+            setSeconds(20);
+            setWorkout(true);
+          }
+        } else {
+          setSeconds(seconds - 1)
+        }
+      } else {
+        setSeconds(10);
+      }
+    }, 1000);
+  }, [seconds, workout, buttonMessage])
+
+  const toggleTimer = () => {
+    if (buttonMessage === "Start") {
+      setButtonMessage("Reset");
+    } else {
+      setButtonMessage("Start");
+      setSeconds(10);
+    }
+  }
 
   return (
     <div className="App">
-      <h1 className='pageTitle'></h1>
-      <TabataTimer userWorkout = {userWorkout} />
-      <Exercises/>
+      <div className="tabataTimer">
+        <div className="timer">{seconds}</div>
+        <button className="startButton" onClick={toggleTimer}>{buttonMessage}</button>
+      </div>
+      <div className='exercisesContainer'>
+        Drag exercises from the list below to build your workout
+        <ul className="exercisesList">
+          {exercises.map(({name, thumbnail}) => {
+            return (
+              <li key={name}>
+                  <div className="characters-thumb">
+                    <img src={thumbnail} alt={`${name} Thumb`} />
+                  </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
